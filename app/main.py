4dfe2401d1af08321ccc3a003b7afdab632bb3ef
Jasper Anders <jasper.anders@online.de>
2021-10-03 14:14:31 +0200

    added slack auth

--- app/main.py
+++ app/main.py
@@ -2,18 +2,26 @@ from flask import Flask, request
 from app.coffedatematching import compute_coffee_dates
 
 app = Flask(__name__)
+version = "v1"
 
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return 'Find coffee matching under /match'
+    return "Find coffee matching under /match"
 
 
-@app.route('/matching', methods=['POST'])
+@app.route("/matching", methods=["POST"])
 def matching():
     requ = request.json
 
     matches = compute_coffee_dates(requ["people"], 2)
 
     print(matches)
-    return {'matches': matches}
+    return {"matches": matches}
+
+
+@app.route(f"/{version}/slack", methods=["POST"])
+def slack_hello():
+    requ = request.json
+
+    return {"challenge": requ["challenge"]}
